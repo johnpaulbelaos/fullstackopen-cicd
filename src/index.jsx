89ecc8e61518bfcb70e122beb935c16705d5014c
John <jpmbelaos@gmail.com>
2026-04-06 19:46:45 +0800

    ops: e2e tests

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <App />
+  <Router><App /></Router>
 )
