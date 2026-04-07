7e9b0b6099effeeb40c5178a0b01987d71f99d38
John <jpmbelaos@gmail.com>
2026-04-07 15:12:48 +0800

    ops: health check

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const start = async () => {
 };
 
 app.get('/version', (req, res) => {
-  res.send('2'); // change this string to ensure a new version deployed
+  res.send('3'); // change this string to ensure a new version deployed
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok');
 });
 
 start();
